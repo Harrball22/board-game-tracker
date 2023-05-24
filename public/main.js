6d97ad949bb7c1804b485105075427851cdc05d4
Harrison Dutson <totodutson@gmail.com>
2023-05-24 16:27:20 -0600

    added create new game button on index.html

---
+++ public/main.js
@@ -0,0 +1,7 @@
+const newGameButton = document.getElementById("new-game-button")
+
+newGameButton.addEventListener("click", createNewGame)
+
+function createNewGame(){
+    console.log("new game")
+}
